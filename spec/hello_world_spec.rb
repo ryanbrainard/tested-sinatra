dcc4bce922e33d962556cddff094840db5c76447
Ryan Brainard <ryan.brainard@gmail.com>
2013-03-11 20:53:10 -0700

    hello goodbye

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -14,6 +14,13 @@ describe 'The HelloWorld App' do
   it "says hello" do
     get '/'
     last_response.should be_ok
+    last_response.body.should == 'Hello World'
+  end
+
+  it "says bye" do
+    get '/'
+    last_response.should be_ok
     last_response.body.should == 'Goodbye World'
   end
 end
+
